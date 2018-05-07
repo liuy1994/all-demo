6858f49cd1e15cd755d0be9de9ce28ac7746bedc
liuy1994 <liuy1994@outlook.com>
2018-05-07 17:21:34 +0800

    change device

--- react/my-app/src/index.js
+++ react/my-app/src/index.js
@@ -3,36 +3,55 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 class Clock extends React.Component {
-  constructor(props){
-    super(props)
-    this.state = {
-      date: new Date(),
-      name: '2333333',
+    constructor(props) {
+        super(props)
+        this.state = {
+            date: new Date(),
+            name: '2333333',
+            isToggleOn: false
+        }
     }
-  }
-  componentDidMount(){
-    this.timeId = setInterval(
-      ()=> this.tick(),1000
-    )
-  }
-  componentWillUnmount() {
-    clearInterval(this.timeId)
-  }
-  tick(){
-    this.setState({
-      date:new Date()
-    })
-  }
-  render(){
-    return (
-      <div>
-      <h1>Hello, {this.state.name}</h1>
-      <h1>It is {this.state.date.toLocaleTimeString()}</h1>
-    </div>
-    )
-  }
+
+    componentDidMount() {
+        this.timeId = setInterval(
+            () => this.tick(), 1000
+        )
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.timeId)
+    }
+
+    tick() {
+        this.setState({
+            date: new Date()
+        })
+    }
+    click(e){
+        e.preventDefault()
+        console.log('This link was cliked!')
+    }
+
+    handleClick = () => {
+        this.setState(prevState => {
+            isToggleOn: !prevState.isToggleOn
+            console.log(this.state.isToggleOn)
+        })
+    }
+    render() {
+        return (
+            <div>
+                <h1>Hello, {this.state.name}</h1>
+                <a href="index.css" onClick={this.click}>CLICK</a>
+                <hr/>
+                <button onClick={this.handleClick}>CLICK</button>
+                <h1>It is {this.state.date.toLocaleTimeString()}</h1>
+            </div>
+        )
+    }
 }
+
 ReactDOM.render(
-  <Clock />,
-  document.getElementById('root')
+    <Clock/>,
+    document.getElementById('root')
 )
